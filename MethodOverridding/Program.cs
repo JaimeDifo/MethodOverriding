8d75be02b0fb32d677d08bc694aa8e7a2b67b171
Jaime A Difo Lorenzo <77880449+JaimeDifo@users.noreply.github.com>
2023-09-28 10:12:44 +0100

    Add files via upload

---
+++ MethodOverridding/Program.cs
@@ -0,0 +1,22 @@
+﻿using MethodOverridding;
+using System.Collections.Generic;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>
+            {
+                new Shape { Width = 100, Height = 100, Type = ShapeType.Circle },
+                new Shape { Width = 100, Height = 100, Type = ShapeType.Rectangle }
+            };
+
+
+            var canvas = new Canvas();
+            canvas.DrawShapes(shapes);
+        }
+    }
+}
+//Update 28092023 - change
